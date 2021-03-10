04bf5bf3ffeec12b2bda2506f62bed6208a6ce36
YPushkareeev <yakov.pushkarev@icloud.com>
2021-03-11 01:31:11 +0300

    hello_world.cpp has been changed

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+string str;
+cout << "Your name:";
+getline(cin, str);
+cout<< "Hello world "<< str <<endl;
+
+ return 1;
+}
