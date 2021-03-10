b266b425a1375d1928fea3d61fa160af5ad31dfe
YPushkareeev <yakov.pushkarev@icloud.com>
2021-03-11 01:58:51 +0300

    some new updates to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 std::string str;
 std::cout << "Your name:";
 getline(cin, str);
-std::cout<< "Hello world "<< str <<endl;
+std::cout<< "Hello world "<< str <<endl;// program will say "hello,world,(your name)"
 
  return 1;
 }
